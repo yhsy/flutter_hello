f947b85bd4e9324f0089150d7eadf591d91e4487
mac <>
2019-12-04 13:40:55 +0800

    1-项目初始化

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+//主函数（入口函数），下面我会简单说说Dart的函数
+void main() =>runApp(MyApp());
+// 声明MyApp类
+class MyApp extends StatelessWidget{
+  //重写build方法
+  @override
+  Widget build(BuildContext context){
+    //返回一个Material风格的组件
+   return MaterialApp(
+      title:'Welcome to Flutteraa',
+      home:Scaffold(
+        //创建一个Bar，并添加文本
+        appBar:AppBar(
+          title:Text('Welcome to Flutteraaaa'),
+        ),
+        //在主体的中间区域，添加一个hello world 的文本
+        body:Center(
+          child:Text('Hello World！！！'),
+        ),
+      ),
+    );
+  }
+}
