8833653e6e5cc07e9ff91c21f3a30bdbc070c8a0
mac <>
2019-12-05 14:41:16 +0800

    4-图片组件及常用属性

--- lib/main.dart
+++ lib/main.dart
@@ -42,35 +42,52 @@ class MyApp extends StatelessWidget {
           //   ),
           // ),
           // 容器
-          child: Container(
-            child: new Text(
-              'Hello Container',
-              style: TextStyle(fontSize: 40.0,color: Colors.white),
+          // child: Container(
+          //   child: new Text(
+          //     'Hello Container',
+          //     style: TextStyle(fontSize: 40.0,color: Colors.white),
+          //   ),
+          //   //  整体对齐(9种对齐方式(顶部中部底部，左中右))
+          //   alignment: Alignment.topLeft,
+          //   width: 320,
+          //   height: 400,
+          //   // 统一设置margin
+          //   margin: const EdgeInsets.all(30),
+          //   // 统一设置padding
+          //   // padding: const EdgeInsets.all(20),
+          //   // 分开设置padding
+          //   padding:const EdgeInsets.fromLTRB(10,30,0,0),
+          //   // color: Colors.lightGreen,
+          //   // 用来设置背景色和边框
+          //   // 注意:设置背景色就要去掉color
+          //   decoration:new BoxDecoration(
+          //     // 渐变背景
+          //     gradient:const LinearGradient(
+          //       colors:[Colors.lightBlue,Colors.greenAccent,Colors.purple]
+          //     ),
+          //     // 纯色背景
+          //     // color: Colors.lightGreen[500],
+          //     // 边框
+          //     border:Border.all(width:10,color:Colors.red)
+          //   ),
+          // ),
+          // 图片
+          child:Container(
+            child:new Image.network(
+              'https://flutter-img-1252835029.cos.ap-shanghai.myqcloud.com/paypal-128.png',
+              // scale:1.0,
+              // 图片拉伸方式(伸缩/挤压等)
+              fit:BoxFit.cover,
+              // 要混合的颜色
+              color: Colors.greenAccent,
+              // 图片混合模式
+              colorBlendMode: BlendMode.darken,
+              // 图片平铺方式
+              repeat: ImageRepeat.repeatY,
             ),
-            //  整体对齐(9种对齐方式(顶部中部底部，左中右))
-            alignment: Alignment.topLeft,
-            width: 320,
-            height: 400,
-            // 统一设置margin
-            margin: const EdgeInsets.all(30),
-            // 统一设置padding
-            // padding: const EdgeInsets.all(20),
-            // 分开设置padding
-            padding:const EdgeInsets.fromLTRB(10,30,0,0),
-            // color: Colors.lightGreen,
-            // 用来设置背景色和边框
-            // 注意:设置背景色就要去掉color
-            decoration:new BoxDecoration(
-              // 渐变背景
-              gradient:const LinearGradient(
-                colors:[Colors.lightBlue,Colors.greenAccent,Colors.purple]
-              ),
-              // 纯色背景
-              // color: Colors.lightGreen[500],
-              // 边框
-              border:Border.all(width:10,color:Colors.red)
-            ),
-          ),
+            width:300,
+            height: 300,
+          )
         ),
       ),
     );
